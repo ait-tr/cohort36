b9aebb652658af5510be465e65a4358bbdfaedfe
Hypericum90 <142326174+Hypericum90@users.noreply.github.com>
2024-03-20 10:16:26 +0400

    Delete lesson_15 directory

--- lesson_15/code/cat-app/index.js
+++
@@ -1,8 +0,0 @@
-const catFactsElement = document.getElementById("cat-facts")
-
-fetch("https://catfact.ninja/fact")
-.then ((response)=>response.json())
-.then((obj)=>{
-    const {fact, length} = obj // деструктуризация
-    catFactsElement.textContent = `${fact} ${length}`
-})
--- lesson_15/code/dog-app/index.js
+++
@@ -1,9 +0,0 @@
-const dogImageElement = document.getElementById("dog-image");
-
-fetch("https://dog.ceo/api/breeds/image/random")
-.then ((response)=>response.json())
-.then ((obj)=>{
-    const {message} = obj
-    console.log(message)
-    dogImageElement.src = message;
-})
