f6959d59cba1bca4a3f26a5195d5e17fbc074c32
Hypericum90 <142326174+Hypericum90@users.noreply.github.com>
2024-03-20 09:30:30 +0400

    Delete lesson_11 directory

--- lesson_11/code/spreadObj.js
+++
@@ -1,5 +0,0 @@
-// Мы можем скопировать поля -значения из одного объекта в другой
-// Spread объекта
-const user = {name:"Igor"}
-const userWithEmail = {...user, email:"example@gmail.com"};
-console.log(userWithEmail);
