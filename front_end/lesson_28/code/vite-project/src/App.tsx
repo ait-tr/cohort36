89264dc8eccfd776e21eff5087b33d2aab5ef102
MIKHAIL <142326174+Hypericum90@users.noreply.github.com>
2024-04-23 01:04:00 +0400

    add files

---
+++ front_end/lesson_28/code/vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Counter from './features/counter/Counter'
+
+function App():JSX.Element {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
