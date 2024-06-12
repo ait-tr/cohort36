54d57105b04eb81ade16807266a7b3667ba55957
mohnapne <kuvaldin2012@gmail.com>
2024-06-12 22:26:26 +0200

    lesson 38

---
+++ front_end/lesson_38/code/my-react/src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import './button.css';
+function Button({buttonText, isGetButton}) {
+
+  return (
+    <button
+      className={`button ${isGetButton ? 'button-danger' : 'button-primary'}`}
+    >
+      {buttonText}
+    </button>
+  );
+}
+
+export default Button;
+
+// сделайте так, чтобы при значении true isGetButton кнопки были одного цвета, а при значении false другого
