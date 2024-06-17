87a45c5c9ade9f9ff98b23a528d4f5393afa51f0
mohnapne <kuvaldin2012@gmail.com>
2024-06-17 22:35:29 +0200

    lesson 40

---
+++ front_end/lesson_40/code/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Lesson06 from './lessons/lesson_06/Lesson06';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Lesson06 />
+  </>
+);
